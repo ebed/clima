ff44bce5039f0a02a5591bf26bc801707f4d3683
Rodrigo Merino <ebedmail@gmail.com>
2018-10-19 15:43:19 -0300

    Probando en ambiente productivo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
+ 
+
 $localizaciones = Redis::Namespace.new("api.localizaciones", :redis => Redis.new)
 $data = Redis::Namespace.new("api.data", :redis => Redis.new)
 $errores = Redis::Namespace.new("api.errors", :redis => Redis.new)
