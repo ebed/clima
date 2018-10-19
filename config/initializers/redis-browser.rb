42bd99849b954c6a1f1c8bd9f0b54fabc1684b51
Rodrigo Merino <ebedmail@gmail.com>
2018-10-19 14:22:39 -0300

    Probando en ambiente productivo

--- config/initializers/redis-browser.rb
+++ config/initializers/redis-browser.rb
@@ -1,5 +1,9 @@
+config = Rails.root.join('config', 'redis-browser.yml')
+settings = YAML.load(ERB.new(IO.read(config)).result)
+RedisBrowser.configure(settings)
+
 RedisBrowser::Web.class_eval do
   use Rack::Auth::Basic, "Protected Area" do |username, password|
-    username == 'foo' && password == 'bar'
+    username == 'test' && password == '123'
   end
 end
