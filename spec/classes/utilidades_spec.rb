0e45822d8533ed871dfe3980e0883093199bee94
Rodrigo Merino <ebedmail@gmail.com>
2018-10-19 20:11:15 -0300

    Se ajusta nuevamente gema de sqlite3 para desarrollo y qa

---
+++ spec/classes/utilidades_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe Utilidades  do
+  it "debe retornar la hora actual" do 
+  end
+
+  it "debe retornar la url de la api de forecast" do 
+  end
+
+  it "Debe calcular la temperatura de farenheit a Celcious" do 
+  end
+
+end
